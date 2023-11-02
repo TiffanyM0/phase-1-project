34720587ee667e4b0fe4860429be5450bc0e1f73
TIFFANY MAINA <tiffmaina@gmail.com>
2023-11-02 16:22:24 +0300

    Linking button

--- public/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaaded", () => {});
-
-function createRequest(word){
-  fetch(`https://wordsapiv1.p.mashape.com/words/${word}`)
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data);
-    });
-}
---
+++ public/src/index.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded",() => {
+  let button = document.querySelector("#searchButton");
+  button.addEventListener("click", (e) => {
+    let searchInput = document.querySelector("#search");
+    let word = searchInput.value;
+    console.log(word);
+
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+
+        let result = document.querySelector("#wordDescription");
+        result.innerHTML = `<p>${data[0].meanings[0].definitions[0].definition}`;
+        console.log(result)
+
+        // let output = data;
+        // console.log(output);
+      });
+
+
+
+    /* link searchInut text to fetch request = print result */
+    // const url = `https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=bright`;
+    // const options = {
+    //   method: "GET",
+    //   headers: {
+    //     "X-RapidAPI-Key": "c6079870camsh1a56f6e78d1aed1p1df6e4jsn0ba2ff61ebfe",
+    //     "X-RapidAPI-Host": "dictionary-by-api-ninjas.p.rapidapi.com",
+    //   },
+    // };
+    // try {
+    //   const response = await fetch(url, options);
+    //   const result = await response.text();
+    //   // console.log(result);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  });
+});
