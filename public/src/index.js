281c2f893224d9ebf4bbc068e244fe542ab5d8d7
TIFFANY MAINA <tiffmaina@gmail.com>
2023-11-06 17:48:16 +0300

    If statements added

--- public/src/index.js
+++ public/src/index.js
@@ -5,34 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
     let word = searchInput.value;
     console.log(word);
     wordSearch(word);
-    // fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-    //   .then((resp) => resp.json())
-    //   .then((data) => {
-    //     console.log(data);
-
-    //     let result = document.querySelector("#wordDescription");
-    //     result.innerHTML = `<p>${data[0].meanings[0].definitions[0].definition}`;
-    //     console.log(result)
-
-    //     // let output = data;
-    //     // console.log(output);
-    //   });
-    /* link searchInut text to fetch request = print result */
-    // const url = `https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=bright`;
-    // const options = {
-    //   method: "GET",
-    //   headers: {
-    //     "X-RapidAPI-Key": "c6079870camsh1a56f6e78d1aed1p1df6e4jsn0ba2ff61ebfe",
-    //     "X-RapidAPI-Host": "dictionary-by-api-ninjas.p.rapidapi.com",
-    //   },
-    // };
-    // try {
-    //   const response = await fetch(url, options);
-    //   const result = await response.text();
-    //   // console.log(result);
-    // } catch (error) {
-    //   console.error(error);
-    // }
   });
 });
 
@@ -46,24 +18,17 @@ function wordSearch(word) {
       result.innerHTML = `
       <p class="return"> 
       ${word} ${data[0].phonetic} <br>
-      ${data[0].meanings[0].partOfSpeech} <br>
-        definition: <br> ${data[0].meanings[0].definitions[0].definition}</p>
-      <p id=synonym class="return">${data[0].meanings[1].partOfSpeech} 
-      <br> ${data[0].meanings[1].definitions[0].definition}</p>
+      ${data[0].meanings[0].partOfSpeech} : ${data[0].meanings[0].definitions[0].definition}</p>
+      <p id=synonym class="return"></p>
       `;
       if(data[0].meanings[1].definitions[0].definition == undefined){
         console.log("undefined")
       } else{
         let meaning = document.querySelector("#synonym");
         console.log(meaning)
-        meaning.innerHTML = `<p> ${data[0].meanings[1].partOfSpeech}<br>${data[0].meanings[1].definitions[0].definition} <p>`
+        meaning.textContent = `${data[0].meanings[1].partOfSpeech} : ${data[0].meanings[1].definitions[0].definition}`
       }
-      console.log(data[0].meanings[1].definitions[0].definition)
-      console.log(data[0].meanings[1].partOfSpeech)
-      // for (let i = 0; i < 3, i++; ) {}
-      // console.log(result);
-
-      // let output = data;
-      // console.log(output);
+      // console.log(data[0].meanings[1].definitions[0].definition)
+      // console.log(data[0].meanings[1].partOfSpeech)
     });
 }
